bc0038c99abf4cfbfe8707c1a5a1733eeedfc60c
cerceda-aaron <aaroncerceda@hotmail.com>
2025-07-03 16:06:09 -0500

    init

---
+++ budget-service/src/test/java/com/budget/budgetservice/BudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
